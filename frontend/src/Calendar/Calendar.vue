82cd303784286fcbcf16077f74a64ce7d55a7556
Kevin Nagels <k.nagels@groupeonepoint.com>
2020-11-22 23:03:43 +0100

    Add Chose calendar format button
    Starting implement of adding a new event in the calendar

--- frontend/src/Calendar/Calendar.vue
+++ frontend/src/Calendar/Calendar.vue
@@ -4,6 +4,12 @@
 		<h1>Mon calendrier</h1>
 		<calendar-view
 			:show-date="showDate"
+			:displayPeriodUom="format"
+			:items="item"
+			displayWeekNumbers=true
+			enableDateSelection=true
+			weekdayNameFormat="long"
+			showTimes=true
 			class="theme-default holiday-us-traditional holiday-us-official">
 			<calendar-view-header
 				slot="header"
@@ -15,10 +21,19 @@
         <p>
             <router-link to="/">Back to home page</router-link>
         </p>
+		<label>Choose calendar format : </label>
+		<select v-model="format" id="formatPicker" name="format">
+			<option value="week">week</option>
+			<option value="month">month</option>
+			<option value="year">year</option>
+		</select>
+		<br>
+		<button v-on:click="addItem">New event</button>
         </div>
 	</div>
 </template>
 <script>
+import { format } from 'path'
 	import { CalendarView, CalendarViewHeader } from "vue-simple-calendar"
 	// The next two lines are processed by webpack. If you're using the component without webpack compilation,
 	// you should just create <link> elements for these. Both are optional, you can create your own theme if you prefer.
@@ -28,7 +43,11 @@
 	export default {
 		name: 'Calendar',
 		data: function() {
-			return { showDate: new Date() }
+			return { showDate: new Date(), format: "month", item:/*store calendar event*/ [{
+				id:0,
+				startDate: new Date(),
+				title:"My first event",
+			}] }
 		},
 		components: {
 			CalendarView,
@@ -38,15 +57,18 @@
 			setShowDate(d) {
 				this.showDate = d;
 			},
+			addItem(){
+				alert("TODO");
+			}
 		}
 	}
 </script>
 <style>
 	#app {
 		font-family: 'Avenir', Helvetica, Arial, sans-serif;
-		color: #2c3e50;
-		height: 100%;
-		width: 100%;
+		color: #502c2c;
+		height: 200%;
+		width: 200%;
 		margin-left: auto;
 		margin-right: auto;
 	}

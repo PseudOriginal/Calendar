1aeda9384aeeb69694973d9b47766ccf337b3a32
Yefan Guo <guo.yefan@hotmail.fr>
2020-12-06 17:14:10 +0100

    update Calendar front version 1.1.0

--- frontend/src/Calendar/Calendar.vue
+++ frontend/src/Calendar/Calendar.vue
@@ -1,9 +1,12 @@
 <template>
 	<div id="app">
 		<div class="calendar-controls">
-			<div v-if="message" class="notification is-success">{{ message }}</div>
 
 			<div class="box">
+				<div class="field">
+					<router-link to="/">Back to home page</router-link>
+				</div>
+
 				<h4 class="title is-5">Options</h4>
 
 				<div class="field">
@@ -20,7 +23,7 @@
 				</div>
 
 				<div class="field">
-					<label class="label">Starting day of the week</label>
+					<label class="label">Starting weekday</label>
 					<div class="control">
 						<div class="select">
 							<select v-model="startingDayOfWeek">
@@ -37,8 +40,8 @@
 				</div>
 
 				<div class="field">
+					<br>
 					<label class="checkbox">
-						<br>
 						<input v-model="displayWeekNumbers" type="checkbox" />
 						Display week numbers
 					</label>
@@ -50,60 +53,67 @@
 						Display hours
 					</label>
 				</div>
+
+				<div class="field">
+					<label class="label">Title*</label>
+					<div class="control">
+						<input v-model="newItemTitle" class="input-text" type="text" />
+					</div>
+				</div>
+
+				<div class="field">
+					<label class="label">Description</label>
+					<div class="control">
+						<textarea v-model="newDescription" class="input-text"/>
+					</div>
+				</div>
+
+				<div class="field">
+					<label class="label">Start date*</label>
+					<div class="control">
+						<input v-model="newItemStartDate" class="input-date" type="date" />
+						<input v-model="newItemStartTime" class="input-time" type="time" />
+					</div>
+				</div>
+
+				<div class="field">
+					<label class="label">End date*</label>
+					<div class="control">
+						<input v-model="newItemEndDate" class="input-date" type="date" />
+						<input v-model="newItemEndTime" class="input-time" type="time" />
+					</div>
+				</div>
+				
+				<br>	
+				<div v-if="!eventSelectionState">
+					<button class="button is-info" @click="addItem">
+						Add Item
+					</button>
+				</div>
+				<div v-else class="btnContainer">
+  					<button class="button is-info" @click="saveEdit">
+						Save Edit
+					</button>
+					<button class="button is-info" @click="cancel">
+						Cancel
+					</button>
+					<button class="button is-info" @click="deleteItem">
+						Delete
+					</button>
+				</div>
 			</div>
 
-			<div class="box">
-				<div class="field">
-					<label class="label">Title</label>
-					<div class="control">
-						<input v-model="newItemTitle" class="input" type="text" />
-					</div>
-				</div>
+			<br>
+			<div v-if="message" class="notification is-success">{{ message }}</div>
 
-				<div class="field">
-					<label class="label">Start date</label>
-					<div class="control">
-						<input v-model="newItemStartDate" class="input" type="date" />
-					</div>
-				</div>
 
-				<div class="field">
-					<label class="label">Start time</label>
-					<div class="control">
-						<input v-model="newItemStartTime" class="input" type="time" />
-					</div>
-				</div>
-
-				<div class="field">
-					<label class="label">End date</label>
-					<div class="control">
-						<input v-model="newItemEndDate" class="input" type="date" />
-					</div>
-				</div>
-
-				<div class="field">
-					<label class="label">End time</label>
-					<div class="control">
-						<input v-model="newItemEndTime" class="input" type="time" />
-					</div>
-				</div>
-
-				<br>
-				<button class="button is-info" @click="addItem">
-					Add Item
-				</button>
-
-				<p>
-					<br>
-					<router-link to="/">Back to home page</router-link>
-				</p>
-			</div>
 		</div>
+
 		<div class="calendar-parent">
 			<calendar-view
 				:items="items"
 				:show-date="showDate"
-				:time-format-options="{ hour: 'numeric', minute: '2-digit' }"
+				:time-format-options="{ hour: '2-digit', minute: '2-digit' }"
 				:show-times="showTimes"
 				:display-period-uom="displayPeriodUom"
 				:starting-day-of-week="startingDayOfWeek"
@@ -143,13 +153,14 @@ import {
 } from "vue-simple-calendar" // published version
 import config from '../_helpers/server.config.js'
 import {authHeader} from '../_helpers/auth-header.js'
-import axios from 'axios';
+import axios from 'axios'
+
 
 export default {
 	name: "Calendar",
 	components: {
 		CalendarView,
-		CalendarViewHeader,
+		CalendarViewHeader
 	},
 	mixins: [CalendarMathMixin],
 	data() {
@@ -164,10 +175,12 @@ export default {
 			selectionStart: null,
 			selectionEnd: null,
 			newItemTitle: "",
+			newItemDescription: "",
 			newItemStartDate: "",
 			newItemStartTime: "",
 			newItemEndDate: "",
 			newItemEndTime: "",
+			eventSelectionState: false,
 			newId: 0,
 			items: [],
 		}
@@ -187,6 +200,15 @@ export default {
 		this.newItemEndTime = "23:59"
 	},
 	methods: {
+		reset(d) {
+			this.eventSelectionState = false
+			this.newItemTitle = ""
+			this.newItemDescription = ""
+			this.newItemStartDate = this.isoYearMonthDay(d)
+			this.newItemStartTime = "00:00"
+			this.newItemEndDate = this.isoYearMonthDay(d)
+			this.newItemEndTime = "23:59"
+		},
 		ignoreTimeZoneIssue(date){
 			return new Date(date.getTime() - date.getTimezoneOffset() * 60000)
 		},
@@ -205,7 +227,7 @@ export default {
 			}
 			axios(request).then(response=>{
 				this.items=response.data
-				this.message= `Event fetched between: ${newPeriod.displayFirstDate.toLocaleDateString()} and ${newPeriod.displayLastDate.toLocaleDateString()}`
+				//this.message= `Event fetched between: ${newPeriod.displayFirstDate.toLocaleDateString()} and ${newPeriod.displayLastDate.toLocaleDateString()}`
 			}).catch(error=>this.message=JSON.stringify(error.response.data))
 		},
 		thisMonth(d, h, m) { // set datetime
@@ -213,15 +235,27 @@ export default {
 			return new Date(t.getFullYear(), t.getMonth(), d, h || 0, m || 0)
 		},
 		onClickDay(d) { // show message when day clicked
+			this.reset(d)
 			this.selectionStart = null
 			this.selectionEnd = null
-			this.message = `You clicked: ${d.toLocaleDateString()}`
+			//this.message = `You clicked: ${d.toLocaleDateString()}`
 		},
 		onClickItem(e) { // show message when event clicked
-			this.message = `You clicked: ${e.title}`
+			this.eventSelectionState = true
+			this.newItemTitle = e.title//${e.startDate}
+			this.newItemDescription = e.description
+
+			this.newItemStartDate = this.isoYearMonthDay(e.startDate)
+			this.newItemEndDate = this.isoYearMonthDay(e.endDate)
+			this.newItemStartTime = (e.startDate.getHours() < 10 ? "0"+e.startDate.getHours() : e.startDate.getHours())
+			+":"+(e.startDate.getMinutes() < 10 ? "0"+e.startDate.getMinutes() : e.startDate.getMinutes())
+			this.newItemEndTime = (e.endDate.getHours() < 10 ? "0"+e.endDate.getHours() : e.endDate.getHours())
+			+":"+(e.endDate.getMinutes() < 10 ? "0"+e.endDate.getMinutes() : e.endDate.getMinutes())
+			//this.message = `${e.startDate}`
 		},
 		setShowDate(d) { // show message for period changed
-			this.message = `Changing calendar view to ${d.toLocaleDateString()}`
+			//this.message = `Changing calendar view to ${d.toLocaleDateString()}`
+			this.eventSelectionState = false
 			this.showDate = d
 		},
 		setSelection(dateRange) { // can select multiple days
@@ -242,13 +276,14 @@ export default {
 		},
 		addItem() { // add a calendar event
 			try{
+				this.newItemStartTime = this.newItemStartTime != "00:00" ? this.newItemStartTime : "00:00:01" // show 00:00 if midnight
 				let newStartDate = new Date(this.newItemStartDate + " " + this.newItemStartTime)
 				let newEndDate = new Date(this.newItemEndDate + " " + this.newItemEndTime)
 				const newEvent = {
 					startDate: this.ignoreTimeZoneIssue(newStartDate).toISOString(),
 					endDate: this.ignoreTimeZoneIssue(newEndDate).toISOString(),
 					title: this.newItemTitle,
-					description: "",
+					description: this.newItemDescription,
 				}
 				const request = {
 					url: config.DEFAULT_ROUTE + "/event/createEvent",
@@ -262,18 +297,32 @@ export default {
 						this.items.push({
 							startDate: newStartDate,
 							endDate: newEndDate,
-							title: this.newItemTitle,
-							description: "",
+							title: newItemTitle,
+							description: newItemDescription,
 							id: this.newId
 						})
 						this.message = "You added a calendar item!"
-					}).catch(error => this.message = JSON.stringify(error.response.data))
+					}).catch(error => this.message = JSON.stringify(error.response.data.message))
 			}
 			catch(e){
 				this.message = e
 				return
 			}
 		},
+		saveEdit() {
+			alert('Save edit function yet to implement')
+			// TODO: implement update function
+		},
+		cancel() {
+			alert('Cancel function yet to implement')
+			// TODO: implement cancel function 
+			// when a field is edited, warning popup shows up
+			// otherwise return to add event interface
+		},
+		deleteItem() {
+			alert('Delete function yet to implement')
+			// TODO: implement delete function
+		}
 	},
 }
 </script>
@@ -311,6 +360,22 @@ body {
 	max-height: 80vh;
 	background-color: white;
 }
+.input-text {
+	width: 202px
+}
+.input-date {
+	width: 120px
+}
+.input-time {
+	width: 80px
+}
+.btnContainer {
+	display: flex;
+	flex-direction: row;
+	width: 202px;
+	justify-content: space-between;
+}
+
 /* For long calendars, ensure each week gets sufficient height. The body of the calendar will scroll if needed */
 .cv-wrapper.period-month.periodCount-2 .cv-week,
 .cv-wrapper.period-month.periodCount-3 .cv-week,

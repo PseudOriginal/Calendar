b9d042151eeb9c4cf997c20ec1cb40d715616cf1
Yefan Guo <guo.yefan@hotmail.fr>
2020-12-15 20:25:07 +0100

    clean code + refonte

--- calendar-backend-app/services/event.service.js
+++ calendar-backend-app/services/event.service.js
@@ -1,138 +1,148 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const db = require('../_helpers/db');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const db = require("../_helpers/db");
 const { Op } = require("sequelize");
-const icalparser = require('ical');
-const icalgen = require('ical-generator');
+const icalparser = require("ical");
+const icalgen = require("ical-generator");
 const fs = require("fs");
 
 module.exports = {
-    createEvent,
-    getEvents,
-    modifyEvent,
-    deleteEvent,
-    importEvents,
-    exportEvents
+  createEvent,
+  getEvents,
+  modifyEvent,
+  deleteEvent,
+  importEvents,
+  exportEvents,
+};
+
+async function createEvent(
+  { startDate, endDate, title, notify, description },
+  email
+) {
+  let event = new db.Event({
+    startDate: startDate,
+    endDate: endDate,
+    title: title,
+    notify: notify,
+    description: description,
+    email: email,
+  });
+
+  if ((event = await event.save())) {
+    const dailyPostman = require("../emails/emails.js");
+    dailyPostman.addDailyMail(event, email);
+    return event;
+  }
+  return null;
 }
 
-async function createEvent({ startDate, endDate, title, notify, description }, email) {
-    let event = new db.Event({startDate: startDate, endDate: endDate, title: title, notify: notify, description: description, email: email});
-
-    if(event = await event.save()) {
-        const dailyPostman = require('../emails/emails.js')
-        dailyPostman.addDailyMail(event, email)
-        return event;
-    }
-    return null;
-}
-
-async function getEvents({ startDate, endDate}, email, checkNotify) {
-    if (!checkNotify) {
-        const events = await db.Event.findAll({
-            where:{
-                email: email,
-                startDate:{
-                    [Op.between]: [startDate, endDate]
-                }
-            }
-        });
-
-        return events;
-    }
-    else {
-        const events = await db.Event.findAll({
-            where:{
-                email: email,
-                notify: true,
-                startDate:{
-                    [Op.between]: [startDate, endDate]
-                }
-            }
-        });
-
-        return events;
-    }
-}
-
-async function modifyEvent({ id, startDate, endDate, title, description }, email) {
-    let event = await db.Event.findOne({
-        where: {
-            id: id,
-            email: email
-        } 
+async function getEvents({ startDate, endDate }, email, checkNotify) {
+  if (!checkNotify) {
+    const events = await db.Event.findAll({
+      where: {
+        email: email,
+        startDate: {
+          [Op.between]: [startDate, endDate],
+        },
+      },
     });
 
-    event.startDate = startDate;
-    event.endDate = endDate;
-    event.title = title;
-    event.notify = notify;
-    event.description = description;
-    
-    if (await event.save()) {
-        const dailyPostman = require('../emails/emails.js')
-        dailyPostman.modifyDailyMail(event, email)
-        return event;
-    }
-    return null;
+    return events;
+  } else {
+    const events = await db.Event.findAll({
+      where: {
+        email: email,
+        notify: true,
+        startDate: {
+          [Op.between]: [startDate, endDate],
+        },
+      },
+    });
+
+    return events;
+  }
+}
+
+async function modifyEvent(
+  { id, startDate, endDate, title, description },
+  email
+) {
+  let event = await db.Event.findOne({
+    where: {
+      id: id,
+      email: email,
+    },
+  });
+
+  event.startDate = startDate;
+  event.endDate = endDate;
+  event.title = title;
+  event.notify = notify;
+  event.description = description;
+
+  if (await event.save()) {
+    const dailyPostman = require("../emails/emails.js");
+    dailyPostman.modifyDailyMail(event, email);
+    return event;
+  }
+  return null;
 }
 
 async function deleteEvent({ id }, email) {
-    let event = await db.Event.findOne({
-        where: {
-            id: id,
-            email: email
-        } 
-    });
+  let event = await db.Event.findOne({
+    where: {
+      id: id,
+      email: email,
+    },
+  });
 
-    if(await event.destroy())
-        return true;
-    return false;
+  if (await event.destroy()) return true;
+  return false;
 }
 
 async function importEvents(file, email) {
-    if(!file)
-    {
-        console.log("File not found")
-        return null;
+  if (!file) {
+    console.log("File not found");
+    return null;
+  }
+  const data = icalparser.parseFile(file.path);
+  var importedEvents = [];
+  for (let item in data) {
+    if (data.hasOwnProperty(item)) {
+      var icalevent = data[item];
+      if (icalevent.type == "VEVENT") {
+        var newEvent = {
+          startDate: icalevent.start,
+          endDate: icalevent.end,
+          title: icalevent.summary,
+          description: icalevent.description,
+        };
+        importedEvents.push(await createEvent(newEvent, email));
+      }
     }
-    const data = icalparser.parseFile(file.path);
-    var importedEvents = [];
-    for (let item in data) {
-        if (data.hasOwnProperty(item)) {
-            var icalevent = data[item];
-            if (icalevent.type == 'VEVENT') {
-                var newEvent = {
-					startDate: icalevent.start,
-					endDate: icalevent.end,
-					title: icalevent.summary,
-					description: icalevent.description
-                }
-                importedEvents.push(await createEvent(newEvent, email));
-            }
-        }
-    }
-    return importedEvents;
+  }
+  return importedEvents;
 }
 
 async function exportEvents(email) {
-    const events = await db.Event.findAll({
-        where:{
-            email: email
-        }
+  const events = await db.Event.findAll({
+    where: {
+      email: email,
+    },
+  });
+  calendarFile = icalgen({
+    domain: "calendarefrei.herokuapp.com",
+    name: "C@lender",
+  });
+  for (let i in events) {
+    var event = events[i];
+    calendarFile.createEvent({
+      start: event.startDate,
+      end: event.endDate,
+      summary: event.title,
+      description: event.description,
     });
-    calendarFile = icalgen({domain:'unlien', name:'C@lender'});
-    for(let i in events)
-    {
-        var event = events[i];
-        calendarFile.createEvent({
-            start: event.startDate,
-            end: event.endDate,
-            summary: event.title,
-            description: event.description
-        })
-    }
-    var filepath = __dirname + "\\storage\\calendar.ics";
-    calendarFile.saveSync(filepath)
+  }
 
-    return filepath;
+  return calendarFile;
 }
--- frontend/src/home/HomePage.vue
+++ frontend/src/home/HomePage.vue
@@ -12,37 +12,19 @@
 </template>
 
 <script>
-/* 
-<h3>Users from secure api end point:</h3>
-    <em v-if="users.loading">Loading users...</em>
-    <span v-if="users.error" class="text-danger">ERROR: {{users.error}}</span>
-    <ul v-if="users.items">
-        <li v-for="user in users.items" :key="user.id">
-            {{user.email}}
-            <span v-if="user.deleting"><em> - Deleting...</em></span>
-            <span v-else-if="user.deleteError" class="text-danger"> - ERROR: {{user.deleteError}}</span>
-            <span v-else> - <a @click="deleteUser(user.id)" class="text-danger">Delete</a></span>
-        </li>
-    </ul>
-*/
-
 import { mapState, mapActions } from 'vuex'
 
 export default {
     computed: {
         ...mapState({
-            account: state => state.account,
-            //users: state => state.users.all
+            account: state => state.account
         })
     },
     created () {
-        //this.getAllUsers();
+
     },
     methods: {
-        /*...mapActions('users', {
-            getAllUsers: 'getAll',
-            deleteUser: 'delete'
-        })*/
+
     }
 };
 </script>
